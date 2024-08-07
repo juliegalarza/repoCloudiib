48ec1a91511c3ab09797b78017f63328dbdc2584
juliegalarza <julie.galarza@epn.edu.ec>
2024-08-07 15:15:04 -0500

    cambio nombre

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ app.set('view engine', 'ejs');
 // middlewares
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
-    host: 'proyectocloudiddb.cwe2eqjpdvxk.us-east-1.rds.amazonaws.com',
+    host: 'pclouddb.cdq7gv9mnn36.us-east-1.rds.amazonaws.com',
     user: 'admin',
     password: 'cloud2024',
     port: 3306,
-    database: 'pclouddb'
+    database: 'pclouddb1'
 }, 'single'));
 app.use(express.urlencoded({extended: false}));
 
